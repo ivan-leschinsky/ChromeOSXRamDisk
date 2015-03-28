28ab8f83faff05d59f718dcc55ec8c29dcdcbf34
Ivan Leschinsky <vanopiano@gmail.com>
2015-03-29 00:24:07 +0300

    Change ram disk size to 384mb

--- ChromeRamDisk.sh
+++ ChromeRamDisk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Size at the end is * 2048 where 2048 = 1 MB, so 2097152 = 1024 MB
-DISK=`/usr/bin/hdiutil attach -nobrowse -nomount ram://2097152`
+DISK=`/usr/bin/hdiutil attach -nobrowse -nomount ram://786432`
 /usr/sbin/diskutil erasevolume HFS+ "RamDiskCache" $DISK
 /bin/rm -rvf ~/Library/Caches/Google/Chrome/*
 /bin/mkdir -pv /Volumes/RamDiskCache/Google
